f7d9d04edaa2a238f8e1a8fabbf2be56ca1d80cf
robin3317 <abdurrahmanrobin47@gmail.com>
2018-06-05 02:20:40 +0600

    set up for basic functionality

--- app.js
+++ app.js
@@ -2,3 +2,42 @@ const express = require("express"),
   app = express(),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser");
+
+//APP CONFIG
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//MONGOOSE CONFIG
+let blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://images.unsplash.com/photo-1501707305551-9b2adda5e527?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0cf5887247e17503ce4e542d00d86b9d&auto=format&fit=crop&w=500&q=60",
+//   body: "This is test blog"
+// });
+
+//RESTFUL ROUTES
+app.get("/", function (req, res) {
+  res.redirect("/blogs");
+})
+app.get("/blogs", function (req, res) {
+  res.render("index");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running");
+});
